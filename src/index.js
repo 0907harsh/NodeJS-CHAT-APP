12c12bd8315db933893770837aa7bdc5f72f93c4
Harsh Gupta <2000harshgupta@gmail.com>
2020-05-23 00:03:41 +0530

    INIT COMMIT @

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app=express()
 const server=http.createServer(app)
 const io=socketio(server)
 
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 const publicDirPath=path.join(__dirname,'../public')
 var qs = require('qs');
 
